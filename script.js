c0f31293e7815c9ff5b627aed9f9e03564db0cb6
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-08-09 10:26:11 +0200

    Create function that creates grid elements
    Created function that takes silder value and creates the proper ammount
    of columns and rows.

--- script.js
+++ script.js
@@ -3,7 +3,34 @@
 const slider = document.querySelector('#slider');
 const divSliderValue = document.querySelector('#sliderValue');
 divSliderValue.textContent = `${slider.value} x ${slider.value}`;
+const container = document.querySelectorAll('#container');
+
 
 slider.addEventListener('input', (e) => {
   divSliderValue.textContent = `${e.target.value} x ${e.target.value}`;
 });
+
+
+
+
+
+function createDivs(num) {
+
+  for (let index = 0; index < num; index++) {
+    const gridColum = document.createElement('div');
+    
+
+    for (let i = 0; i < num; i++) {
+      const columnChild = document.createElement('div');
+      gridColum.appendChild(columnChild);
+
+      
+    }
+
+    container[0].appendChild(gridColum);
+  }
+
+
+}
+
+createDivs(16);
