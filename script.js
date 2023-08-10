1e4f7247297d6b56fe33e1718b8b36b92de0fbe4
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-08-10 09:37:59 +0200

    Add pen color and draw feature
    Added the ability to choose color and draw.

--- script.js
+++ script.js
@@ -4,38 +4,54 @@ const slider = document.querySelector('#slider');
 const divSliderValue = document.querySelector('#sliderValue');
 divSliderValue.textContent = `${slider.value} x ${slider.value}`;
 const container = document.querySelector('#container');
+const colorPicker = document.querySelector('#colorPicker')
+let gridElements = document.querySelectorAll('.gridElement');
+
 
 
 slider.addEventListener('input', (e) => {
   divSliderValue.textContent = `${e.target.value} x ${e.target.value}`;
   container.replaceChildren();
   createGrid(slider.value);
+  
 });
-
-
-
 
 
 function createGrid(num) {
 
-  
-
   // Creates grid column and gives each column a class of 'column'
   for (let index = 0; index < num; index++) {
-    const gridColum = document.createElement('div');
-    gridColum.classList.add('column')
+    const gridColumn = document.createElement('div');
+    gridColumn.classList.add('column')
     
     //Populates columns with gridElements and gives each gridElement a class of 'gridElement'
     for (let i = 0; i < num; i++) {
       const columnChild = document.createElement('div');
       columnChild.classList.add('gridElement')
-      gridColum.appendChild(columnChild);
+
+      //Prevents element from being dragged. Without it hover effect gets stuck on element being dragged
+      columnChild.addEventListener("dragstart", (event) => {
+        event.preventDefault();
+      });
+
+      gridColumn.appendChild(columnChild);
 
       
     }
 
-    container.appendChild(gridColum);
+    container.appendChild(gridColumn);
   }
+
+  gridElements = document.querySelectorAll('.gridElement');
+  for (let index = 0; index < gridElements.length; index++) {
+    const element = gridElements[index];
+    element.addEventListener('click', (e)=>{
+    e.target.style.cssText = `background-color: ${colorPicker.value};`; //must change to dynamic option, assign color every time pencolor changes. fix this.
+    console.log('works')
+  })
+  
+}
+
 
 
 }
