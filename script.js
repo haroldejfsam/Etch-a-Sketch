3061cebaba1e6567aebb97e0f97c07a4ea5d7f39
Ricky Sam <50199974+haroldejfsam@users.noreply.github.com>
2023-08-11 22:49:20 +0200

    Add functionality:pen,eraser,clear,grid
    Added functionality for pen, eraser, clear and grid buttons.

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-// Display range slider value
-
-const slider = document.querySelector('#slider');
-const divSliderValue = document.querySelector('#sliderValue');
+const slider = document.querySelector("#slider");
+const divSliderValue = document.querySelector("#sliderValue");
 divSliderValue.textContent = `${slider.value} x ${slider.value}`;
-const container = document.querySelector('#container');
-const colorPicker = document.querySelector('#colorPicker')
-let gridElements = document.querySelectorAll('.gridElement');
+const container = document.querySelector("#container");
+const colorPicker = document.querySelector("#colorPicker");
+const btnGrid = document.querySelector("#btnGrid");
+const btnClear = document.querySelector("#btnClear");
+const btnEraser = document.querySelector("#btnEraser");
+const btnPen = document.querySelector("#btnPen");
+let gridElements = document.querySelector(".gridElement");
+let drawState = false;
 
-
-
-slider.addEventListener('input', (e) => {
+slider.addEventListener("input", (e) => {
   divSliderValue.textContent = `${e.target.value} x ${e.target.value}`;
   container.replaceChildren();
   createGrid(slider.value);
-  
 });
 
-
 function createGrid(num) {
-
   // Creates grid column and gives each column a class of 'column'
   for (let index = 0; index < num; index++) {
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add('column')
-    
+    const gridColumn = document.createElement("div");
+    gridColumn.classList.add("column");
+
     //Populates columns with gridElements and gives each gridElement a class of 'gridElement'
     for (let i = 0; i < num; i++) {
-      const columnChild = document.createElement('div');
-      columnChild.classList.add('gridElement')
+      const columnChild = document.createElement("div");
+      columnChild.classList.add("gridElement");
 
       //Prevents element from being dragged. Without it hover effect gets stuck on element being dragged
       columnChild.addEventListener("dragstart", (event) => {
@@ -35,26 +33,82 @@ function createGrid(num) {
       });
 
       gridColumn.appendChild(columnChild);
-
-      
     }
 
     container.appendChild(gridColumn);
   }
-
-  gridElements = document.querySelectorAll('.gridElement');
-  for (let index = 0; index < gridElements.length; index++) {
-    const element = gridElements[index];
-    element.addEventListener('click', (e)=>{
-    e.target.style.cssText = `background-color: ${colorPicker.value};`; //must change to dynamic option, assign color every time pencolor changes. fix this.
-    console.log('works')
-  })
-  
-}
-
-
-
 }
 
 createGrid(slider.value);
+
+//Listens to mouse left button is down and sets the draw state
+window.addEventListener("mousedown", function (e) {
+  if (e.button === 0) {
+    drawState = true;
+  }
+});
+
+//Listens to mouse left button is up and sets the draw state
+window.addEventListener("mouseup", function (e) {
+  if (e.button === 0) {
+    drawState = false;
+  }
+});
+
+// Pen button listener
+btnPen.addEventListener("click", () => {
+  gridElements = document.querySelectorAll(".gridElement");
+  for (let index = 0; index < gridElements.length; index++) {
+    const element = gridElements[index];
+    element.addEventListener("mouseenter", (e)=> {
+      if (drawState) {
+        e.target.style.cssText = `background-color: ${colorPicker.value};`;
+      }
+    });
+  
+  };
+  for (let index = 0; index < gridElements.length; index++) {
+    const element = gridElements[index];
+    element.addEventListener("click", (e)=> {
+      
+      if (!drawState) {
+        e.target.style.cssText = `background-color: ${colorPicker.value};`;
+      }
+    });
+  
+  }
+});
+
+// Eraser button listener
+btnEraser.addEventListener("click", () => {
+  gridElements = document.querySelectorAll(".gridElement");
+  for (let index = 0; index < gridElements.length; index++) {
+    const element = gridElements[index];
+    element.addEventListener("mouseenter", (e) => {
+      if (drawState) {
+        e.target.style.cssText = `background-color: white;`;
+      }
+    });
+    element.addEventListener("click", (e) => {
+      if (!drawState) {
+        e.target.style.cssText = `background-color: white;`;
+      }
+    });
+  }
+});
+
+// Grid button listener
+btnGrid.addEventListener("click", () => {
+  gridElements = document.querySelectorAll(".gridElement");
+  for (let index = 0; index < gridElements.length; index++) {
+    const element = gridElements[index];
+    element.classList.toggle("gridOn");
+  }
+});
+
+//Clear button listener
+btnClear.addEventListener("click", () => {
+  container.replaceChildren();
+  createGrid(slider.value);
+});
 
